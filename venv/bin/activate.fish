10f0e2421a2c6531a1df016afc50598f0fcef05d
SammyMwaura <mwaurasammy500@gmail.com>
2024-11-13 21:48:54 +0300

    fix errors

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mgridge/Development/code/phase-5/cloudy/venv"
+set -gx VIRTUAL_ENV "/home/sammy/backend/cloudy/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
