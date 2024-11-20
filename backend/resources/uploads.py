5584055fe81164de8f889e635d6135a89ec1a43e
Ian Maina Ndung'u <mainaianndungu@gmail.com>
2024-11-21 00:18:07 +0300

    Update  cors uploads.py

--- backend/resources/uploads.py
+++ backend/resources/uploads.py
@@ -69,4 +69,5 @@ def add_cors_headers(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+     response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
